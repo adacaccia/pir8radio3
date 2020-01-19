6a22a9806301ea2813dceb75aa712c1073ca6cf7
Adalberto <adacaccia@gmail.com>
2020-01-19 22:13:28 +0100

    multi-process safe grazie al __name__=__main__...

--- pir8radio3.py
+++ pir8radio3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-from subprocess import check_call, Popen
-from gpiozero import Button
+import os, signal, subprocess, time
 from signal import pause
-from time import sleep
-import os, signal, subprocess
+from gpiozero import Button
 from apscheduler.schedulers.background import BackgroundScheduler
 #
 # Global VLC status
@@ -11,11 +9,11 @@ VLC_RUNNING=False
 #
 # proper shutdown button
 def shutdown():
-    check_call(['sudo', 'poweroff'])
+    subprocess.run(['sudo','poweroff'],check=True)
 #
 # check running pid
 def pidof(cmd):
-    cp = subprocess.run(["pidof", cmd],check=True,capture_output=True,text=True)
+    cp = subprocess.run(["pidof",cmd],check=True,capture_output=True,text=True)
     if cp.returncode:
         return 0
     else:
@@ -29,9 +27,8 @@ def run_vlc():
         # we're in parent: do nothing
         VLC_RUNNING=True
     else:
-        # child process: exec the actual work!
-        os.system("/usr/bin/vlc -I dummy http://onair15.xdevel.com:7936")
-        exit
+        # child process: do the actual work!
+        subprocess.run(["/usr/bin/vlc","-I","dummy","http://onair15.xdevel.com:7936"],check=True,capture_output=True,text=True)
 #
 # music on/off button
 def vlc_toggle():
@@ -48,25 +45,27 @@ def vlc_toggle():
             VLC_RUNNING=True
     else:
         run_vlc()
-#   
-# WaveShare's WM8960-Audio-HAT has got a button on GPIO#17
-wm8960_btn = Button(17, hold_time=3)
-wm8960_btn.when_held = shutdown
-wm8960_btn.when_released = vlc_toggle
 #
-# main: start vlc
-run_vlc()
-#
-# start clock
-os.system("~/digital_clock.sh &")
-#
-# Impostazione sveglie come da crontab della pir8radio2:
-# m h  dom mon dow   command
-#30 06	*   *	1-5	$HOME/vlc_send.sh play
-scheduler = BackgroundScheduler()
-scheduler.start()
-scheduler.add_job(vlc_toggle, trigger='cron', minute=30, hour=6, day_of_week='0-4')
-scheduler.add_job(vlc_toggle, trigger='cron', minute=00, hour=19, day_of_week='0-6')
-#
-# wait for next event
-pause()
+# Thread-safe initialization
+if __name__ == '__main__':
+    # WaveShare's WM8960-Audio-HAT has got a button on GPIO#17
+    wm8960_btn = Button(17, hold_time=3)
+    wm8960_btn.when_held = shutdown
+    wm8960_btn.when_released = vlc_toggle
+    #
+    # main: start vlc
+    run_vlc()
+    #
+    # start clock
+    os.system("~/digital_clock.sh &")
+    #
+    # Impostazione sveglie come da crontab della pir8radio2:
+    # m h  dom mon dow   command
+    #30 06	*   *	1-5	$HOME/vlc_send.sh play
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+    scheduler.add_job(vlc_toggle, trigger='cron', minute=30, hour=6, day_of_week='0-4')
+    scheduler.add_job(vlc_toggle, trigger='cron', minute=00, hour=19, day_of_week='0-6')
+    #
+    # wait for next event
+    pause()
